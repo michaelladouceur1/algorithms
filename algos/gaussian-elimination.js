063726e63e0762ce3956d54cda7e37c61c83c584
Michael <michaelladouceur1@gmail.com>
2021-08-30 11:52:35 -0500

    gaussian-elimination solves systems of equations; needs refactoring

--- algos/gaussian-elimination.js
+++ algos/gaussian-elimination.js
@@ -4,17 +4,21 @@
 //   [9, 7, 10],
 // ];
 
+// X = ["x", "y", "z"];
+
 // B = [10, 3, 87];
 
-// A = [
-//   [1, 2, 3, 4, 5],
-//   [2, 5, 0, 9, 8],
-//   [8, 0, 8, 3, 4],
-//   [2, 3, 1, 2, 3],
-//   [3, 5, 4, 6, 0],
-// ];
+A = [
+  [1, 2, 3, 4, 5],
+  [2, 5, 0, 9, 8],
+  [8, 0, 8, 3, 4],
+  [2, 3, 1, 2, 3],
+  [3, 5, 4, 6, 0],
+];
 
-// B = [10, 12, 42, 0, 13];
+X = ["X1", "X2", "X3", "X4", "X5"];
+
+B = [10, 12, 42, 0, 13];
 
 function combineArrays(a, b) {
   for (let i = 0; i < a.length; i++) {
@@ -24,6 +28,20 @@ function combineArrays(a, b) {
 }
 
 function reduceArray(a) {
+  function multiplyBaseArray(a, j, mutator) {
+    for (let i = 0; i < a[0].length; i++) {
+      a[j][i] *= mutator;
+    }
+    return [...a];
+  }
+
+  function addArrays(a, i, j) {
+    for (let k = 0; k < a[0].length; k++) {
+      a[i][k] += a[j][k];
+    }
+    return [...a];
+  }
+
   for (let i = 1; i < a.length; i++) {
     for (let j = 0; j < i; j++) {
       const multiplier = -1 * (a[i][j] / a[j][j]);
@@ -39,25 +57,50 @@ function reduceArray(a) {
   return [...a];
 }
 
-function multiplyBaseArray(a, j, mutator) {
-  for (let i = 0; i < a[0].length; i++) {
-    a[j][i] *= mutator;
+function separateArray(a) {
+  const b = [];
+  for (let i = 0; i < a.length; i++) {
+    b.push(a[i][a[i].length - 1]);
+    a[i] = a[i].slice(0, a[i].length - 1);
   }
-  return [...a];
+  return [a, b];
 }
 
-function addArrays(a, i, j) {
-  for (let k = 0; k < a[0].length; k++) {
-    a[i][k] += a[j][k];
+function solveSystem(a, b) {
+  console.log(a);
+  console.log(b);
+  const solved = {};
+  solved[X[X.length - 1]] = b[b.length - 1];
+
+  for (let i = a.length - 2; i > -1; i--) {
+    let sum = 0;
+    for (let j = a.length - 1; j > i; j--) {
+      sum += solved[X[j]] * a[i][j];
+    }
+    solved[X[i]] = b[i] - sum;
   }
-  return [...a];
+  return solved;
 }
 
-const combined = combineArrays(A, B);
-// console.log(combined);
-const array = reduceArray(combined);
-console.log(
-  array.map((arr) => {
-    return arr.map((el) => Number(el.toFixed(3)));
-  })
-);
+function solve(A, B) {
+  const combined = combineArrays(A, B);
+  const rowEchelon = reduceArray(combined);
+  const [a, b] = separateArray(rowEchelon);
+  const solved = solveSystem(a, b);
+  return solved;
+}
+
+// const combined = combineArrays(A, B);
+// // console.log(combined);
+// const array = reduceArray(combined);
+const solved = solve(A, B);
+console.log("SOLVED: ", solved);
+
+// console.log(
+//   array.map((arr) => {
+//     return arr.map((el) => Number(el.toFixed(3)));
+//   })
+// );
+
+// const sepRE = separateArray(array);
+// console.log(sepRE);
